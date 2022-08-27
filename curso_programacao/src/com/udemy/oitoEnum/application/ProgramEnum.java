5fe2c26a5e9901a37a5c784541285f7fc5c9ab0d
marcospinheiro-dev <marcos.pinheiro.dev@gmail.com>
2022-08-26 22:17:29 -0300

    Commit Oito Exerc Order

--- curso_programacao/src/com/udemy/oitoEnum/application/ProgramEnum.java
+++ curso_programacao/src/com/udemy/oitoEnum/application/ProgramEnum.java
@@ -10,7 +10,7 @@ public class ProgramEnum {
     public static void main(String[] args) {
 
         Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
-
+        System.out.println();
         System.out.println(order);
 
         OrderStatus os1 = OrderStatus.DELIVERED;
---
+++ curso_programacao/src/com/udemy/oitoExercFix/entitie/enums/OrderStatus.java
@@ -0,0 +1,15 @@
+package com.udemy.oitoExercFix.entitie.enums;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED
+
+    //Pagamento pendente
+    //Em processamento
+    //Enviado
+    //Entregue
+
+}
